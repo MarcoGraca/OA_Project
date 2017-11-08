183c151c8b51dad67ddf6505cb63ab2f5d0ee84a
MarcoGraca <marcostbgr16@gmail.com>
2017-11-08 17:41:45 +0000

    Update flowvec.m
    Now works, to test if correct

--- flowvec.m
+++ flowvec.m
@@ -2,16 +2,12 @@ function[f]= flowvec(A,s)
 [~,edges]=size(A);
 [~,inst]=size(s);
 cvx_begin
-    variables f(edges,inst) p(edges)
-    minimize( norm(p,1) )
+    variables f(edges,inst)
+    minimize( norm(max(f,[],edges),1) )
     subject to
     for i = 1:inst
         0<= f(:,i) <= 1;
         A*f(:,i)== -s(:,i);
     end
-    p=max(f,[],edges);
-%     for i=1:edges
-%         p(i) = norm(f(i,:),Inf);
-%     end
 cvx_end
 end
