199da0ea3c45ee318c1f375ea581c1f3494bfddd
MarcoGraca <marcostbgr16@gmail.com>
2017-11-08 16:54:03 +0000

    Updated flowvecs

--- flowvec.m
+++ flowvec.m
@@ -1,14 +1,17 @@
-[nodes,edges]=size(A);
+function[f]= flowvec(A,s)
+[~,edges]=size(A);
 [~,inst]=size(s);
-s1=s(:,1);
 cvx_begin
     variables f(edges,inst) p(edges)
     minimize( norm(p,1) )
     subject to
-    for i=1:inst
-        0 <= f(:,i) <= 1;
-        A*f(:,i)+s(:,i)=zeros(nodes);
+    for i = 1:inst
+        0<= f(:,i) <= 1;
+        A*f(:,i)== -s(:,i);
     end
     p=max(f,[],edges);
-    
+%     for i=1:edges
+%         p(i) = norm(f(i,:),Inf);
+%     end
 cvx_end
+end
