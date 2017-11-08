981e020c34a3b055c81ba14dd11f1a75e525433e
MarcoGraca <marcostbgr16@gmail.com>
2017-11-08 19:47:01 +0000

    Update flowvecE.m
    Now works but always considers overhead

--- flowvecE.m
+++ flowvecE.m
@@ -1,19 +1,18 @@
-function[f]= flowvec(A,s,c,p,E)
+function[f,tc]= flowvec(A,s,c,p,E)
 [~,edges]=size(A);
 [~,inst]=size(s);
 cvx_begin
-    variables f(edges,inst)
-    r=max(f,[],2);
-    for i = 1:edges
-        if r(i)<10^(-9)
-            E(i)=0;
-        end
-    end
-    minimize(dot(p,r)+sum(E))
-    subject to
-    for i = 1:inst
-        0<= f(:,i) <= c(i);
-        A*f(:,i)== -s(:,i);
-    end
+variables f(edges,inst)
+r=max(f,[],2);
+%     for i = 1:edges
+%         E(i)=E(i)*(r(i)>10^(-9));
+%     end
+tc=dot(p,r)+sum(E);
+minimize(tc)
+subject to
+for i = 1:inst
+    0<= f(:,i) <= c(i);
+    A*f(:,i)== -s(:,i);
+end
 cvx_end
 end
