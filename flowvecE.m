370a3ccc1515f170b3704ef79c30fce0831bd7fa
xadreyux <joao.firmino@tecnico.ulisboa.pt>
2017-11-15 12:43:38 +0000

    works without E. round problem

--- flowvecE.m
+++ flowvecE.m
@@ -1,18 +1,17 @@
-function[f,tc]= flowvec(A,s,c,p,E)
+function[f,r,tc]= flowvec(A,s,c,p,E)
 [~,edges]=size(A);
 [~,inst]=size(s);
 cvx_begin
 variables f(edges,inst)
-r=max(f,[],2);
-%     for i = 1:edges
-%         E(i)=E(i)*(r(i)>10^(-9));
-%     end
-tc=dot(p,r)+sum(E);
+variable r(edges)
+tc=dot(p,r);
 minimize(tc)
 subject to
-for i = 1:inst
-    0<= f(:,i) <= c(i);
-    A*f(:,i)== -s(:,i);
-end
+    r(:) <= c(:);
+    0 <= f(:);
+    for i = 1:inst
+        f(:,i) <= r(:);
+        A*f(:,i) == -s(:,i);
+    end
 cvx_end
 end
