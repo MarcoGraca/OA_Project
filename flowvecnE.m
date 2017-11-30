e1cff5199e8f6e6086f919910a5a1dabd8377bff
MarcoGraca <marcostbgr16@gmail.com>
2017-11-30 19:41:01 +0000

    No Overflow
    For testing purposes

---
+++ flowvecnE.m
@@ -0,0 +1,17 @@
+function[f1,tc]= flowvecnE(A,s,c,p)
+[~,edges]=size(A);
+[~,inst]=size(s);
+cvx_begin
+variable f1(edges,inst)
+variable r(edges)
+tc=dot(p,r);
+minimize(tc)
+subject to
+for i = 1:inst
+    A*f1(:,i) == -s(:,i);
+    0 <= f1(:,i) <= r;
+    r <= c;
+end
+cvx_end
+disp(r);
+end
