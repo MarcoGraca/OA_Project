a7dc91cc59a020e923117843f07ae866f02db0c8
Pedro Vasco <pedro.vasco1996@gmail.com>
2017-12-06 11:56:46 +0000

    add tolerance in overheads code

--- flowvec2.m
+++ flowvec2.m
@@ -1,21 +1,21 @@
-function[f1,f2,tc,r]= flowvec2(A,s,c,p,E)
+function[f,o,tc,r]= flowvec2(A,s,c,p,E)
 [~,edges]=size(A);
 [~,inst]=size(s);
 cvx_begin
-variable f1(edges,inst)
+variable f(edges,inst)
 variable r(edges)
-variable f2(edges)
+variable o(edges)
 %     for i = 1:edges
 %         E(i)=E(i)*(r(i)>10^(-9));
 %     end
-tc=dot(p,r)+dot(f2,E);
+tc=dot(p,r)+dot(o,E);
 minimize(tc)
 subject to
 for i = 1:inst
-    0<= f1(:,i) <= r;
-    r <= f2.*c;
-    0 <= f2 <= 1;
-    A*f1(:,i) + s(:,i) <= 10^{-10};
+    0<= f(:,i) <= r;
+    r <= o.*c;
+    0 <= o <= 1;
+    A*f(:,i) + s(:,i) <= 10^{-10};
 end
 cvx_end
 end
