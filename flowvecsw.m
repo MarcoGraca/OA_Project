7627778e0d96ed60ff23adb24d5aa57f7b66c99a
Pedro Vasco <pedro.vasco1996@gmail.com>
2017-12-08 18:08:53 +0000

    redifine names

--- flowvecsw.m
+++ flowvecsw.m
@@ -1,20 +1,20 @@
-function[tc,f1,f2,r]= flowvecsw(A,s,c,p,t)
+function[tc,f,h,r]= flowvecsw(A,s,c,p,t)
 [edges,~]=size(t);
 [~,inst]=size(s);
 As=A*diag(t);
 cvx_begin
-variable f1(edges,inst)
-variable f2(edges,inst)
+variable f(edges,inst)
+variable h(edges,inst)
 variable r(edges)
 tc=dot(p,r);
 minimize(tc)
 subject to
 for i = 1:inst
-    0 <= f1(:,i) <= r;
-    0 <= f2(:,i) <= r;
+    0 <= f(:,i) <= r;
+    0 <= h(:,i) <= r;
     r <= c;
-    A*f1(:,i) + s(:,i) <= 10^(-10);
-    As*f2(:,i) + s(:,i) <= 10^(-10);
+    A*f(:,i) + s(:,i) <= 10^(-10);
+    As*h(:,i) + s(:,i) <= 10^(-10);
 end
 cvx_end
 end
